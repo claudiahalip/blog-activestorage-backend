4fcdb694a306e4827c8d5456889e673a562eb8f3
claudiahalip <claudia4ro@yahoo.com>
2021-03-01 16:56:15 -0600

    update post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,5 +13,15 @@ class PostsController < ApplicationController
     end
 
     def create 
+        post = Post.create(title: params[:post][:title], content: params[:post][:content])
+        render json: post
+    end
+
+    def update 
+        post = Post.find(params[:id])
+        post.update(featured_image: params[:featured_image])
+        featured_image_url = rails_blob_path(post.featured_image)
+        
+        render json: {post: posts, featured_image: featured_image_url}
     end
 end
