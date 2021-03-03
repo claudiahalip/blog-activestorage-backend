356bde74ee1b5db052182a767ac6bb061ae0dac8
claudiahalip <claudia4ro@yahoo.com>
2021-03-03 16:55:29 -0600

    image url added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,18 @@ class PostsController < ApplicationController
 
     def index
         posts = Post.all.with_attached_featured_image 
-        render json: posts.to_json(include: {featured_image_attachment: {include: :blob}})
+        render json: posts.map { |post| 
+            post.as_json.merge({ image: url_for(post.featured_image) })    
+        }
+
+        #render json: posts.to_json(include: {featured_image_attachment: {include: :blob}})
     end
 
     def show
         post = Post.find(params[:id])
         featured_image = rails_blob_path(post.featured_image)
-        render json: {post: post, featured_image: featured_image}
-    
+        url = url_for(post.featured_image) 
+        render json: {post: post, featured_image: featured_image, url: url}
     end
 
     def create 
@@ -21,7 +25,9 @@ class PostsController < ApplicationController
         post = Post.find(params[:id])
         post.update(featured_image: params[:featured_image])
         featured_image_url = rails_blob_path(post.featured_image)
-        
+         
         render json: {post: posts, featured_image: featured_image_url}
+
+        
     end
 end
