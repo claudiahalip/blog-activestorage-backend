b271be62fd9ebfb2f05ab0932192db4cb90d4a0e
claudiahalip <claudia4ro@yahoo.com>
2021-03-06 10:04:53 -0600

    post_controller updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,6 @@ class PostsController < ApplicationController
         render json: posts.map { |post| 
             post.as_json.merge({ image: url_for(post.featured_image) })    
         }
-
         #render json: posts.to_json(include: {featured_image_attachment: {include: :blob}})
     end
 
@@ -27,7 +26,5 @@ class PostsController < ApplicationController
         featured_image_url = rails_blob_path(post.featured_image)
          
         render json: {post: posts, featured_image: featured_image_url}
-
-        
     end
 end
